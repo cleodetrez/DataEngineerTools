cef0f00ef460a8c1383976e97a9c243f17aa2d76
cleodetrez <cleo.detrez@edu.esiee.fr>
2025-11-25 15:31:17 +0100

    2Scrapy fait

---
+++ 2Scrapy/monprojet/monprojet/spiders/books.py
@@ -0,0 +1,9 @@
+import scrapy
+
+class BooksSpider(scrapy.Spider):
+    name = "books"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["http://books.toscrape.com/"]
+
+    def parse(self, response):
+        pass
